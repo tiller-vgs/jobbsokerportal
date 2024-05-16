8281b94eb0576cb2b5ad0d7783f224f43d9d7d46
AEksel <akselgab@gmail.com>
2024-05-16 13:51:44 +0200

    Job component div appearance
    W.I.P
    #19

--- jobbsokerportal2024/app/components/JobComponent.tsx
+++ jobbsokerportal2024/app/components/JobComponent.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 export default function JobComponent(props: JobComponentProps) {
 	return (
-		<div className='bg-white rounded-sm flex-colum justify-between'>
+		<div className='bg-white rounded-xl flex-colum justify-between pb-10 m-3 mt-5'>
 			<h1 className='text-5xl'>{props.Title}</h1>
 			<p className='mt-5 ml-5'>{props.Desc}</p>
-			<div className='self-center'>
+
+			<div className='text-center -mt-10'>
 				<p>{props.Tag}</p>
 			</div>
 
-			<p className='self-end'>{props.date}</p>
+			<div className='text-end -mt-5 mr-5'>
+				<p className='self-end'>{props.date}</p>
+			</div>
 		</div>
 	);
 }
--- jobbsokerportal2024/app/components/JobListingDiv.tsx
+++ jobbsokerportal2024/app/components/JobListingDiv.tsx
@@ -3,14 +3,28 @@ import JobComponent from './JobComponent';
 
 export default function JobListingDiv() {
 	return (
-		<div className='translate-y-32 h-[86vh] bg-slate-400 overflow-auto'>
-			<JobComponent
-				Title='Placeholder Title'
-				Desc='Placeholder Description'
-				Tag='Placeholder Tag'
-				date='Placeholder  Date'
-				bilde='Placeholder Picture'
-			/>
+		<div className='translate-y-32 h-screen overflow-auto'>
+			<h1 className='text-2xl text-center'>AVAILABLE POSITIONS</h1>
+			<div className='h-[70vh] overflow-auto m-4 border-solid border-2 border-b-gray-500 z-100	'>
+				<JobComponent
+					Title='Placeholder Title'
+					Desc='Placeholder Description'
+					Tag='Placeholder Tag'
+					date='OOOOOOO'
+					bilde='Placeholder Picture'
+				/>
+			</div>
+
+			<h1 className='mt-5 text-2xl text-center'>UNAVAILABE POSITIONS</h1>
+			<div className='h-[70vh] overflow-auto m-4'>
+				<JobComponent
+					Title='Placeholder Title'
+					Desc='Placeholder Description'
+					Tag='Placeholder Tag'
+					date='OOOOOOO'
+					bilde='Placeholder Picture'
+				/>
+			</div>
 		</div>
 	);
 }
